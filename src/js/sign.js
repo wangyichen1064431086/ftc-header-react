a3d0368c56b0d4eb31ee038801e29dddd7d1de7d
wangyichen1064431086 <1064431086@qq.com>
2018-02-07 17:11:50 +0800

    Use es6 to split object

--- src/js/sign.js
+++ src/js/sign.js
@@ -3,8 +3,11 @@ import React from 'react';
 class Sign extends React.Component {
 
   render() {
-    const data = this.props.signData;
-    const hasSigned = this.props.hasSignIn;
+    const {signData,hasSigned,clickSignIn} = this.props;//es6的拆分对象(具体名词叫什么，忘记了。。)
+    //const data = this.props.signData;
+    //const hasSigned = this.props.hasSignIn;
+    const data = signData;
+    console.log(data);
     let signMenu
     if (hasSigned) {
       signMenu = (
@@ -23,7 +26,7 @@ class Sign extends React.Component {
     } else {
       signMenu = (
         <div className="ftc-header__sign-readermenu ftc-header__sign-visitormenu">
-          <a className="ftc-header__sign-signin" href={data.signIn.url} onClick={(e) => this.props.clickSignIn(e)}>
+          <a className="ftc-header__sign-signin" href={data.signIn.url} onClick={(e) => clickSignIn(e)}>
             {data.signIn.word}
           </a>
           <a href={data.signUp.url}>
