4b355109b3684abf9968aeebf42658a056b34ca5
wangyichen1064431086 <1064431086@qq.com>
2018-02-11 18:14:17 +0800

    Change the login form to controlled component

--- src/js/loginOverlay.js
+++ src/js/loginOverlay.js
@@ -1,7 +1,62 @@
 import React from 'react';
 
 class LoginOverlay extends React.Component {
- 
+  constructor(props) {
+    super(props);
+    this.state = {
+      username:'',
+      password:'',
+      saveme:'1'
+    }
+    /*
+    this.handleUsernameChange = this.handleUsernameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleSavemeChange = this.handleSavemeChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    */
+  }
+  /*
+  handleUsernameChange(e) {
+    this.setState({
+      username:e.target.value
+    });
+  }
+  handlePasswordChange(e) {
+    this.setState({
+      password:e.target.value
+    });
+  }
+  handleSavemeChange(e) {
+    this.setState((prevState) => ({
+      saveme:!prevState.saveme
+    }));
+  }
+  */
+  handleChange(fieldname, e) {
+    switch(fieldname) {
+      case 'username':
+      case 'password':
+        const {value} = e.target;
+        this.setState({
+          [fieldname]:value
+        });
+        break;
+      case 'saveme':
+        const {checked} = e.target;
+        let newSavemeValue;
+        if(checked) {
+          newSavemeValue = '1';
+        } else {
+          newSavemeValue = '0';
+        }
+        this.setState({
+          saveme:newSavemeValue
+        });
+        break;
+      default:
+        console.log('Have not dealed with this field');
+    }
+  }
   render () {
     const show = this.props.show;
     let displayClass;
@@ -10,6 +65,7 @@ class LoginOverlay extends React.Component {
     } else {
       displayClass = "ftc-header__loginoverlay";
     }
+    const {username, password, saveme} = this.state;
     return (
       <div className={displayClass} >
         <div className="ftc-header__loginoverlay-window">
@@ -24,18 +80,18 @@ class LoginOverlay extends React.Component {
               <label htmlFor="ftcLoginUsername">
                   电子邮件/用户名
               </label>
-              <input type="text" name="username" id="ftcLoginUsername" />
+              <input type="text" name="username" id="ftcLoginUsername" value={username} onChange = {this.handleChange.bind(this, 'username')} />
             </div>
 
             <div className="ftc-header__loginoverlay-item">
               <label htmlFor="ftcLoginPassword">
                 密码
               </label>
-              <input type="password" className="ftc-header__loginoverlay-oneline" name="password" id="ftcLoginPassword" />
+              <input type="password" className="ftc-header__loginoverlay-oneline" name="password" id="ftcLoginPassword" value={password} onChange = {this.handleChange.bind(this,'password')} />
             </div>
           
             <div className="ftc-header__loginoverlay-saveandsub">
-              <input className="ftc-header__loginoverlay-saveme" type="checkbox" value="1" defaultChecked="checked" name="saveme" id="ftcLoginSaveme" />
+              <input className="ftc-header__loginoverlay-saveme" type="checkbox" value={saveme} checked={saveme==='1'} name="saveme" id="ftcLoginSaveme" onChange={this.handleChange.bind(this,'saveme')}/>
               <label htmlFor="ftcLoginSaveme">记住我</label>
 
               <input className="ftc-header__loginoverlay-submit" type="submit" value="提交" />
